fdd9cddec327e6ada8d57f05ae59a7092d30795d
mrmyothet <mr.myothet@gmail.com>
2023-06-11 17:07:39 +0630

    Install json-server, axios, and Add db.json

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 #! /usr/bin/evn/ node
-console.log("Hello World");
+
+// console.log("Hello World");
+
+const { default: axios } = require("axios");
+const { stdin, stdout } = require("process");
+
+/*
+Run this app from the command line 
+Prompt the user asking what they ate, and how much 
+Show the user how many calories are in that food 
+Setup database 
+*/
+
+const readLine = require("readline").createInterface({
+  input: stdin,
+  output: stdout,
+});
+
+readLine.question(`What would you like to log today? `, async (item) => {
+  const { data } = await axios.get("http://localhost:3001/food");
+
+  const it = data[Symbol.iterator]();
+  let position = it.next();
+  while (!position.done) {
+    const food = position.value.name;
+    console.log(item, food);
+  }
+
+  readLine.close();
+});
